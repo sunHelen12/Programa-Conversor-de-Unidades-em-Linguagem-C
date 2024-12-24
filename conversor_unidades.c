19ecfd3f9496d43e21b9699b7c87ca31460c7928
DanielSOliver <dansant4038@gmail.com>
2024-12-24 03:16:32 -0300

    Update conversor_unidades.c

--- conversor_unidades.c
+++ conversor_unidades.c
@@ -335,6 +335,50 @@ int main() {
                 break;
             }
 
+            case 9: { // Conversão de Dados
+            const double fatorConversao = 8.0; // 8 bits corresponde a 1 byte
+            int unidadeOrigem, unidadeDestino;
+            double valor, resultado;
+
+            printf("Conversor de unidades:\n");
+            printf("0. Bits\n");
+            printf("1. Bytes\n");
+            printf("2. Kilobytes (KB)\n");
+            printf("3. Megabytes (MB)\n");
+            printf("4. Gigabytes (GB)\n");
+            printf("5. Terabytes (TB)\n");
+
+            printf("Escolha o tipo de unidade que voce deseja converter (0-5): ");
+            scanf("%d", &unidadeOrigem);
+
+            printf("Escolha em qual tipo de unidade que voce deseja converter (0-5): ");
+            scanf("%d", &unidadeDestino);
+
+            printf("Digite o valor a ser convertido: ");
+            scanf("%lf", &valor);
+
+            if (unidadeOrigem >= 0 && unidadeOrigem <= 5 && unidadeDestino >= 0 && unidadeDestino <= 5) {
+                resultado = valor;
+
+                if (unidadeOrigem < unidadeDestino) {
+                    for (int i = unidadeOrigem; i < unidadeDestino; i++) {
+                        resultado /= (i == 0 ? fatorConversao : 1024);
+                    }
+                } else if (unidadeOrigem > unidadeDestino) {
+                    for (int i = unidadeOrigem; i > unidadeDestino; i--) {
+                        resultado *= (i - 1 == 0 ? fatorConversao : 1024);
+                    }
+                }
+
+                printf("O resultado da conversao eh: %.2f\n", resultado);
+            } else {
+                printf("Unidades invalidas! Escolha valores entre 0 e 5.\n");
+            }
+
+                break;
+            }
+
+            
             case 10: { // Sair
                 printf("Saindo do programa...\n");
                 break;
